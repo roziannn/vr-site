f28f2f057abf5f73bdd26e193fe36e451581d51c
Firda Rosiana T <firdaroziana10@gmail.com>
2024-11-17 17:48:50 +0700

    added pricing, testimo, workflow and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Feature from "./components/Feature";
+import Workflow from "./components/Workflow";
+import Price from "./components/Price";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
         <Feature />
+        <Workflow />
+        <Price />
+        <Testimonial />
+        <Footer />
       </div>
     </>
   );
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -62,61 +62,51 @@ export const features = [
   {
     icon: <BotMessageSquare />,
     text: "Drag-and-Drop Interface",
-    description:
-      "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
+    description: "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
   },
   {
     icon: <Fingerprint />,
     text: "Multi-Platform Compatibility",
-    description:
-      "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
+    description: "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
   },
   {
     icon: <ShieldHalf />,
     text: "Built-in Templates",
-    description:
-      "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
+    description: "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
   },
   {
     icon: <BatteryCharging />,
     text: "Real-Time Preview",
-    description:
-      "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
+    description: "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
   },
   {
     icon: <PlugZap />,
     text: "Collaboration Tools",
-    description:
-      "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
+    description: "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
   },
   {
     icon: <GlobeLock />,
     text: "Analytics Dashboard",
-    description:
-      "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
+    description: "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
   },
 ];
 
 export const checklistItems = [
   {
     title: "Code merge made easy",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+    description: "Track the performance of your VR apps and gain insights into user behavior.",
   },
   {
     title: "Review code without worry",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+    description: "Track the performance of your VR apps and gain insights into user behavior.",
   },
   {
     title: "AI Assistance to reduce time",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+    description: "Track the performance of your VR apps and gain insights into user behavior.",
   },
   {
     title: "Share work in minutes",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
+    description: "Track the performance of your VR apps and gain insights into user behavior.",
   },
 ];
 
@@ -124,32 +114,17 @@ export const pricingOptions = [
   {
     title: "Free",
     price: "$0",
-    features: [
-      "Private board sharing",
-      "5 Gb Storage",
-      "Web Analytics",
-      "Private Mode",
-    ],
+    features: ["Private board sharing", "5 Gb Storage", "Web Analytics", "Private Mode"],
   },
   {
     title: "Pro",
     price: "$10",
-    features: [
-      "Private board sharing",
-      "10 Gb Storage",
-      "Web Analytics (Advance)",
-      "Private Mode",
-    ],
+    features: ["Private board sharing", "10 Gb Storage", "Web Analytics (Advance)", "Private Mode"],
   },
   {
     title: "Enterprise",
     price: "$200",
-    features: [
-      "Private board sharing",
-      "Unlimited Storage",
-      "High Performance Network",
-      "Private Mode",
-    ],
+    features: ["Private board sharing", "Unlimited Storage", "High Performance Network", "Private Mode"],
   },
 ];
 
